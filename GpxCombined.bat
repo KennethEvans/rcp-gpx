4e4ea5a1751a5d06273eb8150e75e7f72309285e
Kenneth Evans Jr <ken@kenevans.net>
2015-03-19 15:56:09 +0000

    Changed XSD_DIR in GpxCombined.bat. (Was incorrect.)
    git-svn-id: http://svn.kenevans.net/Projects/trunk@746 4b487630-4e8c-464e-b16a-428ddfb157b8

--- GpxCombined.bat
+++ GpxCombined.bat
@@ -1,7 +1,7 @@
 rem BAT file to generate combined JAXB files
 
 set XJC=C:\Java\JDK\jdk1.6.0_20\bin\xjc.exe
-set XSD_DIR=C:\eclipseWorkspaces\Work\net.kenevans.gpxextensions
+set XSD_DIR=C:\eclipseWorkspaces\Work\net.kenevans.gpx
 set DEST_DIR=C:\Scratch\GPX-Combined-XSD
 set PACKAGE=net.kenevans.gpxcombined
 
